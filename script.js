584bfc1c771d24668d0622b02e7c5c1c1682ef45
Samuel <samuelobimbaawesome@gmail.com>
2026-01-31 17:49:02 +0100

    First save

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Grandmaster Chess Club website loaded successfully.');
+
+    // Future interactive functionality will go here
+});
